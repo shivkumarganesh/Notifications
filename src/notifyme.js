0a3a7cb557b2afefe2bb4355df24735073c14782
Arnelle Balane <arnellebalane@gmail.com>
2015-06-29 22:15:46 +0800

    refactored notifyme api

--- src/notifyme.js
+++ src/notifyme.js
@@ -1,116 +1,70 @@
-/**
- * Global Variable NotifyMe
- */
-var NotifyMe = {};
-(function (window, NotifyMe) {
-    var Notify;
-    Notify = (function () {
-        var getPermissionAndLaunch, launchNotiWindow, mergeObjects, defaultOptions, generateGuid;
-
-        /**
-         * Notify Constructor
-         * @param {String} title   Title for Notification
-         * @param {Object} options Object with Options for Notification
-         */
-        function Notify(title, options) {
-            this.title = title;
-            this.options = options;
-        }
-
-        /**
-         * UUID Generator
-         * @return {String} Object  Returns a string of UUID for each of the Notification
-         */
-        generateGuid = function () {
-            function s4() {
-                return Math.floor((1 + Math.random()) * 0x10000)
-                    .toString(16)
-                    .substring(1);
-            }
-            return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
-        };
-        defaultOptions = {
-            icon: '',
-            body: 'This is the Default Body',
-            lang: 'en-US',
-            tag: generateGuid(),
-            dir: 'ltr'
-        };
-
-        /**
-         * Helps in launching Notification(Private)
-         * @param  {String} title   Title for Notification
-         * @param  {Object} options Object with Options for Notification
-         * @return {Notification}         Object of Notification is returned along with the Notification being launched.
-         */
-        launchNotiWindow = function (title, options) {
-            var notify;
-            notify = new Notification(title, options);
-            notify.onclick = function () {
-                options.onclick();
-            };
-            notify.onerror = function () {
-                options.onerror();
-            };
-            notify.onclose = function () {
-                options.onclose();
-            };
-        };
-
-        /**
-         * Helps to merge the two Objects.
-         * defaultOptions refers to the option which is available by default
-         * options refers to the options that are being injected.
-         * @param  {Object} defaultOptions  Object carrying default options
-         * @param  {Object} options Object carrying options provided by the user
-         * @return {Object} defaultOptions  merges options with defaultObject and returns defaultOption
-         */
-        mergeObjects = function (defaultOptions, options) {
-            for (var p in options) {
-                try {
-                    // Property in destination object set; update its value.
-                    if (options[p].constructor == Object) {
-                        defaultOptions[p] = MergeRecursive(defaultOptions[p], options[p]);
-                    } else {
-                        defaultOptions[p] = options[p];
-                    }
-                } catch (e) {
-                    // Property in destination object not set; create it and set its value.
-                    defaultOptions[p] = options[p];
-                }
-            }
-            return defaultOptions;
-        };
-
-        /**
-         * Checks for the permission from the user
-         * @param  {String} title   Title for Notification
-         * @param  {Object} options Object with Options for Notification
-         * @return {NA}         NA
-         */
-        getPermissionAndLaunch = function (title, options) {
-            Notification.requestPermission(function (permission) {
-                if (permission === 'granted') {
-                    launchNotiWindow(title, mergeObjects(defaultOptions, options));
-                } else if (permission === 'denied') {
-                    console.log("Request denied by the User");
-                }
-            });
-        };
-        /**
-         * Prototype function for invoking the Notification object.
-         * @return {NA} [NA]
-         */
-        Notify.prototype.launch = function () {
-            return getPermissionAndLaunch(this.title, this.options);
-        };
-        return Notify;
-    })();
-
-    NotifyMe.launch = function (title, options) {
-        var tempObj = new Notify(title, options);
-        tempObj.launch();
+(function(root, definition) {
+    if (typeof define === 'function' && define.amd) {
+        define([], definition);
+    } else {
+        root.NotifyMe = definition();
+    }
+})(this, function() {
+    var defaultOptions = {
+        icon: 'http://i.istockimg.com/file_thumbview_approve/46749378/3/stock-illustration-46749378-cute-piglet-icon-animal-icons-series.jpg',
+        lang: 'en-US'
     };
 
-    return window.NotifyMe;
-})(this, NotifyMe);
+
+    function extend(base, extension) {
+        var extended = {};
+        for (var key in base) {
+            extended[key] = base[key];
+        }
+        for (var key in extension) {
+            if (extension.hasOwnProperty(key)) {
+                if (extension[key].constructor === Object) {
+                    base[key] = extend(base[key], extension[key]);
+                } else {
+                    base[key] = extension[key];
+                }
+            }
+        }
+        return extended;
+    }
+
+
+    function NotifyMe(title, body, options) {
+        if (!(this instanceof NotifyMe)) {
+            return new NotifyMe(title, body, options);
+        }
+        this.title = title;
+        this.options = extend(defaultOptions, options);
+        this.options.body = body;
+    }
+
+    NotifyMe.permissionGranted = false;
+
+    NotifyMe.requestPermission = function() {
+        return new Promise(function(resolve, reject) {
+            Notification.requestPermission(function(permission) {
+                NotifyMe.permissionGranted = permission === 'granted';
+                if (NotifyMe.permissionGranted) {
+                    resolve();
+                } else {
+                    reject();
+                }
+            });
+        });
+    };
+
+    NotifyMe.prototype.launch = function() {
+        if (NotifyMe.permissionGranted) {
+            this.notification = new Notification(this.title, this.options);
+            this.notification.onclick = this.options.onclick;
+            this.notification.onerror = this.options.onerror;
+            this.notification.onclose = this.options.onclose;
+        } else {
+            NotifyMe.requestPermission().then(this.launch.bind(this));
+        }
+        return this.notification;
+    };
+
+
+    return NotifyMe;
+});
