234e7b89036b42c4321ca8cae8193ed9ade28eef
Arnelle Balane <arnellebalane@gmail.com>
2015-06-30 14:53:06 +0800

    use gulp-jscs-stylish as reporter for jscs errors

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var uglify = require('gulp-uglify');
 var jscs = require('gulp-jscs');
+var stylish = require('gulp-jscs-stylish');
 
 
 gulp.task('compress', function () {
@@ -13,7 +14,9 @@ gulp.task('compress', function () {
 
 gulp.task('lint', function() {
     gulp.src('./src/*.js')
-        .pipe(jscs().on('error', gutil.log))
+        .pipe(jscs())
+        .on('error', gutil.noop)
+        .pipe(stylish())
         .pipe(gulp.dest('./dist'));
 });
 
